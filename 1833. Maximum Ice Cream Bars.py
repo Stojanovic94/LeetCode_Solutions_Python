3889b08ad78770345c9008d141ded4a52145242e
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2026-06-21 07:58:07 +0200

    Add files via upload

---
+++ 1833. Maximum Ice Cream Bars.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxIceCream(self, costs, coins):
+        max_cost = max(costs)
+
+        count = [0] * (max_cost + 1)
+
+        for cost in costs:
+            count[cost] += 1
+
+        bars = 0
+
+        for cost in range(1, max_cost + 1):
+            if count[cost] == 0:
+                continue
+
+            can_buy = min(count[cost], coins // cost)
+
+            bars += can_buy
+            coins -= can_buy * cost
+
+            if coins < cost:
+                break
+
+        return bars
