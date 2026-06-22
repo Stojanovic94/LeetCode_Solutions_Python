6d766e4ef66b9db648d6e716e678a4d44f02de79
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2026-06-22 09:24:10 +0200

    Add files via upload

---
+++ 1189. Maximum Number of Balloons.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        d = defaultdict(int)
+        word = "balloon"
+        for t in text:
+            if t in word:
+                d[t] +=1
+        
+        if any(t not in d for t in word):
+            return 0
+        else:
+            return min(d['b']//1, d['a']//1, d['l']//2, d['o']//2, d['n']//1)
