044f09cb50203c251521f3f4d432d6c4437d7701
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2026-08-30 18:50:24 +0200

    Add files via upload

---
+++ 2091. Removing Minimum and Maximum From Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumDeletions(self, nums: List[int]) -> int:
+        minn = min(nums)
+        maks = max(nums)
+        if minn == maks: return 1
+        minn_i = nums.index(minn)
+        maks_i = nums.index(maks)
+        u = len(nums)
+
+        print(minn_i, maks_i)
+
+        if minn_i > maks_i:
+            minn_i, maks_i = maks_i, minn_i
+
+
+        cevap = min(minn_i + 1 + u-maks_i, max(minn_i, maks_i) + 1, u - min(minn_i, maks_i))
+
+        return cevap
+
+        
